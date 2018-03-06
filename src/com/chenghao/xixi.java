4f86f8acce7c53d4371803524079e523d774ee28
liulifu123 <274025921@qq.com>
2018-03-06 17:29:31 +0800

    idea提交

---
+++ src/com/chenghao/xixi.java
@@ -0,0 +1,14 @@
+package com.chenghao;
+
+public class xixi {
+    public static void main(String[] args){
+        int [] j = {1,2,3,4};
+       /* for (int i = 0; i < j.length; i++) {
+            System.out.println(j[i;])
+        }*/
+        for (int i : j) {
+            System.out.println(i);
+        }
+
+    }
+}
