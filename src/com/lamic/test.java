f19797bca35c6acdb07551bcba2aff45d60fd11b
liulifu123 <274025921@qq.com>
2018-03-07 16:36:39 +0800

    11

--- src/com/lamic/test.java
+++ src/com/lamic/test.java
@@ -3,6 +3,6 @@ package com.lamic;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("1wewq21213312");
-		System.out.println(123);
+		System.out.println(1223);
 	}
 }
